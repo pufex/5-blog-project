b6c1d83bb6bd710d4503b76ee18fb3b7fce1f454
vwwwwwv420 <marbaalex3@gmail.com>
2024-08-01 14:22:53 +0200

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Homepage of my application.",
+};
+
+export default function Home() {
+  return    
+}
